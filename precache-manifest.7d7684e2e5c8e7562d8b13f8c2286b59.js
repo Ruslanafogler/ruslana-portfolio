3f25cbfbdf24146bc2045810f58ade4a94e48be1
rusla <Ruslanafogler@gmail.com>
2020-11-11 22:08:59 -0800

    Updates

---
+++ precache-manifest.7d7684e2e5c8e7562d8b13f8c2286b59.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e41b5c6b8a7498491beb0d98f32d34",
+    "url": "/ruslana-portfolio/index.html"
+  },
+  {
+    "revision": "b5b834f6bc6309e37f27",
+    "url": "/ruslana-portfolio/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "0433588d40c5211b0747",
+    "url": "/ruslana-portfolio/static/css/main.89f06dd5.chunk.css"
+  },
+  {
+    "revision": "b5b834f6bc6309e37f27",
+    "url": "/ruslana-portfolio/static/js/2.5498f7ed.chunk.js"
+  },
+  {
+    "revision": "b5ff4111ab81c8c91e9e746653eb6fff",
+    "url": "/ruslana-portfolio/static/js/2.5498f7ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0433588d40c5211b0747",
+    "url": "/ruslana-portfolio/static/js/main.70eb86e8.chunk.js"
+  },
+  {
+    "revision": "17186d20ad7e70f0970f",
+    "url": "/ruslana-portfolio/static/js/runtime-main.24142990.js"
+  },
+  {
+    "revision": "adf8f6f7d98a3e334dfb0be3b885df2d",
+    "url": "/ruslana-portfolio/static/media/blend.adf8f6f7.svg"
+  },
+  {
+    "revision": "ab589f8bf1961b915fe3c04eb4d9f3b4",
+    "url": "/ruslana-portfolio/static/media/cherryPi.ab589f8b.svg"
+  },
+  {
+    "revision": "f5bf68457a57f92184de28d717939a47",
+    "url": "/ruslana-portfolio/static/media/crashTest.f5bf6845.svg"
+  },
+  {
+    "revision": "0bd240986540795352d2a6a4f1926a8e",
+    "url": "/ruslana-portfolio/static/media/icon.0bd24098.png"
+  },
+  {
+    "revision": "9e4cd8c30bd0ed6fcf5688cb244d46a9",
+    "url": "/ruslana-portfolio/static/media/inkscape.9e4cd8c3.svg"
+  },
+  {
+    "revision": "4142c0453fd0e45026693e0d08ad2386",
+    "url": "/ruslana-portfolio/static/media/me_sunset.4142c045.jpeg"
+  },
+  {
+    "revision": "706146e3bcaf659560e8a12efc7567d3",
+    "url": "/ruslana-portfolio/static/media/mobilemuscle.706146e3.svg"
+  },
+  {
+    "revision": "ded4d5d9cc43a006a7b102de58f8fb7e",
+    "url": "/ruslana-portfolio/static/media/working.ded4d5d9.svg"
+  }
+]);
